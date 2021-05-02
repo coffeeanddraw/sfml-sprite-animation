1f402c3cffeace331f847820d710459cb18abe61
Cattatonicat <threeeyedvenelope@gmail.com>
2021-05-02 16:59:07 -0500

    Open and close window
    Open and close window with SFML

---
+++ SpriteAnimation/Source.cpp
@@ -0,0 +1,15 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+	sf::VideoMode vm(500, 500);
+	sf::RenderWindow window(vm, "Sprite Animation");
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+	}
+}
