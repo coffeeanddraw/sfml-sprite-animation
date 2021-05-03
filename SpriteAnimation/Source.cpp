6eeb948a0f0210a51b8b1473205770953a8d1552
Cattatonicat <threeeyedvenelope@gmail.com>
2021-05-03 01:12:17 -0500

    Add variables

--- SpriteAnimation/Source.cpp
+++ SpriteAnimation/Source.cpp
@@ -1,8 +1,21 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main() {
 	sf::VideoMode vm(500, 500);
 	sf::RenderWindow window(vm, "Sprite Animation");
+
+	sf::Texture animTexture;
+	if (!animTexture.loadFromFile("graphics/eye-sheet-x8.png")) {
+		std::cout << "eye-sheet-x8 missing in graphics folder.";
+		return -1;
+	}
+
+	sf::Vector2i spriteSize(152, 72);
+	std::vector<sf::Sprite> animSprites;
+
+	int sheetRow = animTexture.getSize().y / spriteSize.y;
+	int sheetColumn = animTexture.getSize().x / spriteSize.x;
 
 	while (window.isOpen()) {
 		sf::Event event;
